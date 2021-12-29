ec327edfcddc1e3d6b4c116858bae257ef2a09b2
Markus Digruber <mdigruber@gmail.com>
2021-12-29 15:30:39 +0100

    added cli support and error management

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use md5::{Md5, Digest};
-use std::{fs, io};
+use std::{fs, io, path};
+use std::env;
 
 fn main() -> io::Result<()> {
 
+    let args: Vec<String> = env::args().collect();
+    
+    let cli_path = &args[1];
+    
+    if !path::Path::new(&cli_path).exists() {
+        println!("{:?} does not exist", &cli_path);
+        panic!(" {} does not exist", &cli_path)
+    }
+   
     // Ready Directory where duplicates should be checked
-    let path = fs::read_dir("./images").unwrap();
+    let path = fs::read_dir(cli_path).unwrap();
+
 
     // Vecs are dynamic arrays in Rust 
     let mut arr : Vec<String> = vec![];
@@ -32,5 +43,6 @@ fn main() -> io::Result<()> {
         }
 
     }
+    
     Ok(())
 }
